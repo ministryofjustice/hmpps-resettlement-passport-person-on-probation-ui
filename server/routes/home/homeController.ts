c0fa46e56360f552783cc4602cf062be3e187d5a
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-02-12 11:51:04 +0000

    Add redis configuration

--- server/routes/home/homeController.ts
+++ server/routes/home/homeController.ts
@@ -2,6 +2,15 @@ import { RequestHandler } from 'express'
 
 export default class HomeController {
   index: RequestHandler = async (req, res) => {
-    res.render('pages/index')
+    /**
+     * To be deleted, this code is to test the Redis session is configured correctly
+     */
+    if (!req.session.tempValue) {
+      req.session.tempValue = Math.random()
+    }
+
+    res.render('pages/index', {
+      sessionValue: req.session.tempValue,
+    })
   }
 }
