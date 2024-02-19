a37ac6dc6ef5d86026cf2233cff2c05de66cbbec
riccardo-noviello-moj <159039282+riccardo-noviello-moj@users.noreply.github.com>
2024-02-19 13:55:34 +0000

    Plt 66 Initial OTP Screen without backend integration (#18)
    * uncomment deployment
    
    * exclude landing page from auth
    
    * Add initial OTP screen and stubbed validation
    
    * Add initial OTP integration tests
    
    * Add initial OTP integration tests
    
    * fix linting issue
    
    * fix linting issue
    
    * fix linting issue

---
+++ integration_tests/e2e/otpVerification.cy.ts
@@ -0,0 +1,33 @@
+import OtpPage from '../pages/otp'
+import GovukOneLoginPage from '../pages/govukOneLogin'
+import Page from '../pages/page'
+import DashboardPage from '../pages/dashboard'
+
+context('OTP verification', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.visit('/otp')
+    Page.verifyOnPage(GovukOneLoginPage)
+    cy.signIn()
+  })
+
+  it('Should not continue to Dashboard after validating OTP (invalid)', () => {
+    cy.visit('/otp') // replace with stubbing user verification
+    Page.verifyOnPage(OtpPage)
+
+    cy.get('#otp').type('abcqwe')
+    cy.get('.govuk-button').click()
+    Page.verifyOnPage(OtpPage)
+    cy.contains('There is a problem')
+  })
+
+  it('Should continue to Dashboard after validating OTP (valid)', () => {
+    cy.visit('/otp') // replace with stubbing user verification
+    Page.verifyOnPage(OtpPage)
+
+    cy.get('#otp').type('123456')
+    cy.get('.govuk-button').click()
+    Page.verifyOnPage(DashboardPage)
+  })
+})
---
+++ integration_tests/pages/dashboard.ts
@@ -0,0 +1,7 @@
+import Page from './page'
+
+export default class DashboardPage extends Page {
+  constructor() {
+    super('Dashboard')
+  }
+}
