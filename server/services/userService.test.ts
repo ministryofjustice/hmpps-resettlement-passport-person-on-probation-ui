3a9e5736f3224061d1e003d397402a11f888f7c0
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-02-26 12:06:54 +0000

    fix bug with backend response type wrong

--- server/services/userService.test.ts
+++ server/services/userService.test.ts
@@ -19,18 +19,15 @@ const redisClient = {
 } as unknown as jest.Mocked<RedisClient>
 
 const mockedOtpResponse = {
-  id: 3,
-  prisoner: {
-    id: 2,
-    nomsId: 'G4161UF',
-    creationDate: '2023-08-29T10:49:36.114432',
-    crn: 'U416100',
-    prisonId: 'MDI',
-    releaseDate: '2024-08-01',
-  },
-  creationDate: '2024-02-21T11:14:41.939462',
-  expiryDate: '2024-02-28T23:59:59.939462',
-  otp: 123456,
+  id: 9,
+  crn: 'U416100',
+  cprId: 'NA',
+  email: 'test@example.com',
+  verified: true,
+  creationDate: '2024-02-26T11:58:17.812884699',
+  modifiedDate: '2024-02-26T11:58:17.812884699',
+  nomsId: 'G4161UF',
+  oneLoginUrn: 'urn:fdc:gov.asdasdasd',
 }
 
 const oneLoginTestUrn = 'fdc:gov.uk:2022:asdasdasd-asdasdasd'
