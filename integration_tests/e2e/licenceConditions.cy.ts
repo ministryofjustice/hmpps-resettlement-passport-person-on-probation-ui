c3139f7c162e865b6e38d508c99049937c8fdd24
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-03-08 13:00:03 +0000

    Add appointment tile to dashboard

--- integration_tests/e2e/licenceConditions.cy.ts
+++ integration_tests/e2e/licenceConditions.cy.ts
@@ -28,11 +28,22 @@ context('Licence conditions', () => {
     cy.get('#licence-dates').contains('20 August 2023 - 12 July 2023')
 
     // standard conditions should be visible
-    cy.contains('Be of good behaviour and not behave in a way which undermines the purpose of the licence period.')
+    cy.contains('1. Be of good behaviour and not behave in a way which undermines the purpose of the licence period.')
 
     // additional conditions should be visible
     cy.contains(
-      'Not to enter the area of Leeds City Centre, as defined by the attached map, without the prior approval of your supervising officer.',
+      '2. Not to enter the area of Leeds City Centre, as defined by the attached map, without the prior approval of your supervising officer.',
     )
+  })
+
+  it('Should be able to see the Licence Conditions Tile', () => {
+    cy.signIn()
+
+    // should be on the dashboard
+    cy.contains('John Smith')
+
+    // licence-conditions tile should exist and clickable
+    cy.get('#licence-conditions-tile').should('exist')
+    cy.get('#licence-conditions-tile-body').contains('Your licence conditions expire on 12 July 2023')
   })
 })
