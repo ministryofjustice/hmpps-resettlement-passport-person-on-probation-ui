62a38a7bcb94bf6febdf98f0e57f3e16bf7f6ff0
riccardo-noviello-moj <159039282+riccardo-noviello-moj@users.noreply.github.com>
2024-03-18 14:23:48 +0000

    Add licence details page (#40)

--- server/routes/licenceConditions/index.ts
+++ server/routes/licenceConditions/index.ts
@@ -5,4 +5,5 @@ import LicenceConditionsController from './licenceConditionsController'
 export default (router: Router, services: Services) => {
   const controller = new LicenceConditionsController(services.licenceConditionService, services.userService)
   router.get('/licence-conditions', [controller.index])
+  router.get('/licence-conditions/:licenceId/condition/:conditionId', [controller.view])
 }
