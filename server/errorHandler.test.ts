2e7bc242e960d5145a1fb6f05251745b4fc3cd74
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-04-17 11:36:28 +0100

    PLT-180 Page not found fix tests

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -1,37 +1,31 @@
-import type { Express } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes } from './routes/testutils/appSetup'
 
-let app: Express
-
 beforeEach(() => {
-  app = appWithAllRoutes({})
+  appWithAllRoutes({})
 })
 
 afterEach(() => {
   jest.resetAllMocks()
 })
 
-describe('GET 404', () => {
-  it('should render content with stack in dev mode', () => {
-    return request(app)
-      .get('/unknown')
-      .expect(404)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('NotFoundError: Not Found')
-        expect(res.text).not.toContain('Something went wrong. The error has been logged. Please try again')
-      })
-  })
-
-  it('should render content without stack in production mode', () => {
+describe('GET 500', () => {
+  it('should render error page with a user friendly title in Production mode', () => {
     return request(appWithAllRoutes({ production: true }))
-      .get('/unknown')
-      .expect(404)
+      .get('/dashboard')
+      .expect(500)
       .expect('Content-Type', /html/)
       .expect(res => {
         expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
-        expect(res.text).not.toContain('NotFoundError: Not Found')
+      })
+  })
+  it('should render error page without the user friendly title in Dev mode', () => {
+    return request(appWithAllRoutes({ production: false }))
+      .get('/dashboard')
+      .expect(500)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).not.toContain('Something went wrong. The error has been logged. Please try again')
       })
   })
 })
