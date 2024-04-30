29962bb49441ac209209b154ac59bfcf2a4dc6d1
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-04-30 09:53:08 +0100

    PLT-98: add initial unit test

--- server/middleware/rateLimites.test.ts
+++ server/middleware/rateLimites.test.ts
@@ -1,11 +1,52 @@
+import { rateLimiterMiddleware } from './rateLimiter'
+import { Request, Response, NextFunction } from 'express'
+import { createRequest, createResponse, MockRequest, MockResponse } from 'node-mocks-http'
 
-describe('RateLimiter', () => {
+jest.mock('express')
 
+describe('rateLimiterMiddleware', () => {
+  let reqMock: MockRequest<Request>
+  let resMock: MockResponse<Response>
+  let nextMock: jest.Mocked<NextFunction>
 
-    it('should restrict some paths', ()=> {})
-    it('should allow some paths', ()=> {})
+  beforeAll(() => {
+    jest.useFakeTimers({ advanceTimers: true })
+    jest.setSystemTime(new Date(2023, 8, 2))
+  })
 
-    it('should limit more than 10 requests in 1 minute', ()=> {})
+  beforeEach(() => {
+    resMock = createResponse()
+    nextMock = jest.fn()
+  })
 
-    it('should allow more than 10 requests in 1 minute after latest reset', ()=> {})
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
+  it('should allow some paths', () => {
+    reqMock = createRequest({
+      method: 'GET',
+      url: '/',
+    })
+    rateLimiterMiddleware(reqMock, resMock, nextMock)
+    expect(nextMock).toHaveBeenCalled()
+  })
+
+  it('should restrict some paths', () => {
+    reqMock = createRequest({
+      method: 'POST',
+      url: '/otp/verify',
+    })
+    rateLimiterMiddleware(reqMock, resMock, nextMock)
+    expect(resMock.status).toBe(429)
+    expect(nextMock).not.toHaveBeenCalled()
+  })
+
+  it('should limit more than 10 requests in 1 minute', () => {})
+
+  it('should allow more than 10 requests in 1 minute after latest reset', () => {})
 })
