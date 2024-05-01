e6d2ddd5c0eb1a03c04d03f3d8e84a76d99eec1f
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-05-01 15:37:39 +0100

    PLT-98: setup for zendesk integration and ratelimiter

---
+++ server/middleware/rateLimiter.ts
@@ -0,0 +1,54 @@
+import express, { Router, Request, Response, NextFunction } from 'express'
+
+const rateLimitedPaths = ['/feedback/submit', '/otp/verify']
+
+interface RateLimiter {
+  [ip: string]: {
+    count: number
+    lastRequest: number
+  }
+}
+
+const rateLimiters: RateLimiter = {}
+
+const isWindowLapsed = (now: number, ip: string, window: number) => now - rateLimiters[ip].lastRequest >= window
+
+const isLimitExceeded = (ip: string, limit: number) => rateLimiters[ip].count > limit
+
+export const rateLimiterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (rateLimitedPaths.indexOf(req.path) < 0) {
+    return next()
+  }
+  const { ip } = req
+  const now = Date.now()
+  const limit = 10 // Limit to 10 requests per minute
+  const window = 60000 // 1 minute in milliseconds
+
+  // initialise or increase
+  if (!rateLimiters[ip]) {
+    rateLimiters[ip] = { count: 1, lastRequest: now }
+  } else {
+    rateLimiters[ip].count += 1
+  }
+
+  // reset counter
+  if (isLimitExceeded(ip, limit) && isWindowLapsed(now, ip, window)) {
+    rateLimiters[ip].count = 0
+  }
+
+  rateLimiters[ip].lastRequest = now
+
+  // deny
+  if (isLimitExceeded(ip, limit)) {
+    return res.status(429).send('Too Many Requests')
+  }
+
+  // continue
+  return next()
+}
+
+export function setupRateLimiter(): Router {
+  const router = express.Router()
+  router.use(rateLimiterMiddleware)
+  return router
+}
