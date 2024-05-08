19e8063c84d20b6de531c9496c7b95c876bd0bfd
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-05-08 11:27:22 +0100

    PLT-262: Add session Id to logs and headers

--- server/data/resettlementPassportApiClient.ts
+++ server/data/resettlementPassportApiClient.ts
@@ -19,68 +19,93 @@ export default class ResettlementPassportApiClient {
     this.restClient = new RestClient('Resettlement Passport Api Client', config.apis.resettlementPassportApi)
   }
 
-  async getLicenceConditionsImage(nomsId: string, licenceId: number, conditionId: number): Promise<string> {
+  async getLicenceConditionsImage(
+    nomsId: string,
+    licenceId: number,
+    conditionId: number,
+    sessionId: string,
+  ): Promise<string> {
     try {
+      logger.debug(`SessionId: ${sessionId}. getLicenceConditionsImage(${nomsId}, ${licenceId}, ${conditionId})`)
       const imageResult = await this.restClient.get<string>({
         path: `/prisoner/${nomsId}/licence-condition/id/${licenceId}/condition/${conditionId}/image`,
+        headers: {
+          SessionID: sessionId,
+        },
       })
 
       return Buffer.from(imageResult).toString('base64')
     } catch (error) {
-      logger.error('Licence condition image not found:', error)
+      logger.error(`SessionId: ${sessionId}. Licence condition image not found:`, error)
       checkError(error)
     }
     return null
   }
 
-  async getLicenceConditionsByNomsId(nomsId: string): Promise<LicenceConditionData> {
+  async getLicenceConditionsByNomsId(nomsId: string, sessionId: string): Promise<LicenceConditionData> {
     try {
+      logger.debug(`SessionId: ${sessionId}. getLicenceConditionsByNomsId(${nomsId})`)
       const response = await this.restClient.get<LicenceConditionData>({
         path: `/prisoner/${nomsId}/licence-condition`,
+        headers: {
+          SessionID: sessionId,
+        },
       })
       return response
     } catch (error) {
-      logger.error('Licence conditions not found:', error)
+      logger.error(`SessionId: ${sessionId}. Licence conditions not found:`, error)
       checkError(error)
     }
     return null
   }
 
-  async getByNomsId(nomsId: string): Promise<PersonalDetails> {
+  async getByNomsId(nomsId: string, sessionId: string): Promise<PersonalDetails> {
     try {
+      logger.debug(`SessionId: ${sessionId}. getByNomsId(${nomsId})`)
       const response = await this.restClient.get<PersonalDetails>({
         path: `/prisoner/${nomsId}`,
+        headers: {
+          SessionID: sessionId,
+        },
       })
       return response
     } catch (error) {
-      logger.error('Prisoner not found:', error)
+      logger.error(`SessionId: ${sessionId}. Prisoner not found:`, error)
       checkError(error)
     }
     return null
   }
 
-  async getAppointments(nomsId: string): Promise<AppointmentData> {
+  async getAppointments(nomsId: string, sessionId: string): Promise<AppointmentData> {
     try {
+      logger.debug(`SessionId: ${sessionId}. getAppointments(${nomsId})`)
       const response = await this.restClient.get<AppointmentData>({
         path: `/prisoner/${nomsId}/appointments?futureOnly=false&includePreRelease=false`,
+        headers: {
+          SessionID: sessionId,
+        },
       })
       return response
     } catch (error) {
-      logger.error('Prisoner appointments not found:', error)
+      logger.error(`SessionId: ${sessionId}. Prisoner appointments not found:`, error)
       checkError(error)
     }
     return null
   }
 
-  async submitUserOtp(otpRequest: OtpRequest): Promise<OtpDetailsResponse> {
+  async submitUserOtp(otpRequest: OtpRequest, sessionId: string): Promise<OtpDetailsResponse> {
     try {
+      logger.debug(`SessionId: ${sessionId}. submitUserOtp()`)
       const response = await this.restClient.post<OtpDetailsResponse>({
         path: `/popUser/onelogin/verify`,
         data: otpRequest,
+        headers: {
+          SessionID: sessionId,
+        },
       })
       return response
     } catch (error) {
-      logger.error('OTP not found:', error)
+      logger.error(`SessionId: ${sessionId}. OTP not found:`, error)
       checkError(error)
     }
     return null
--- server/routes/dashboard/dashboardController.ts
+++ server/routes/dashboard/dashboardController.ts
@@ -16,19 +16,23 @@ export default class DashboardController {
   index: RequestHandler = async (req, res, next) => {
     try {
       const urn = req.user?.sub
-      const verificationData = await requireUser(urn, this.userService)
+      const sessionId = req.sessionID
+      const verificationData = await requireUser(urn, this.userService, sessionId)
       if (typeof verificationData === 'string') {
         return res.redirect(verificationData)
       }
-      const personalData = await this.userService.getByNomsId(verificationData.nomsId, urn)
+      const personalData = await this.userService.getByNomsId(verificationData.nomsId, urn, sessionId)
 
-      const appointments = await this.appointmentService.getAllByNomsId(verificationData.nomsId)
+      const appointments = await this.appointmentService.getAllByNomsId(verificationData.nomsId, sessionId)
       const nextAppointment = getFutureAppointments(appointments.results)?.[0]
 
       const tomorrowAppointments = appointments?.results?.filter(x => isTomorrow(new Date(x.date)))
       const todayAppointments = appointments?.results?.filter(x => isToday(new Date(x.date)))
 
-      const licence = await this.licenceConditionsService.getLicenceConditionsByNomsId(verificationData.nomsId)
+      const licence = await this.licenceConditionsService.getLicenceConditionsByNomsId(
+        verificationData.nomsId,
+        sessionId,
+      )
 
       return res.render('pages/dashboard', {
         user: req.user,
