3dcd236db6d1f8280c8235be0701c1a099657dd5
“DAVID MCCOURT <167427411+dmccourtzMoj@users.noreply.github.com>
2024-06-06 16:43:26 +0100

    removed password and key

--- security_test/test_security.py
+++ security_test/test_security.py
@@ -4,12 +4,14 @@ from zapv2 import ZAPv2, reports
 import time
 
 # Change to match the API key set in ZAP, or use None if the API key is disabled
-zap_api_key = "15jae1iqcvcpumvqk4f70ohlq8"
+zap_api_key = ""
 proxy_url = "http://localhost:8181"
 
 
 def run(playwright: Playwright):
     zap = ZAPv2(apikey=proxy_url, proxies={'http': "http://localhost:8181"})
+    zap.core.exclude_from_proxy('https://oidc.integration.account.gov.uk.*')
+    zap.core.exclude_from_proxy('https://signin.integration.account.gov.uk.*')
     chromium = playwright.chromium
     browser = chromium.launch(headless=True, proxy={"server": proxy_url})
     context = browser.new_context(ignore_https_errors=True)
@@ -21,71 +23,50 @@ def run(playwright: Playwright):
 
     page.locator('//*[@id="email"]').fill("testuserpyf+1@gmail.com")
     page.locator('//*[@id="main-content"]/div/div/form/button').click()
-    page.locator('//*[@id="password"]').fill("0net1mepa55")
+    page.locator('//*[@id="password"]').fill("")
     page.locator('//*[@id="form-tracking"]/button').click()
     assert page.inner_text('[data-qa="pyf-overview-h1"]') == "Overview"
 
 
     # Spider the target URL
-
     target_url = "https://person-on-probation-user-ui-dev.hmpps.service.justice.gov.uk/"
 
     print(f"Spidering target URL: {target_url}")
 
+   # zap.spider.exclude_from_scan('https://oidc.integration.account.gov.uk.*')
+   # zap.spider.exclude_from_scan('https://signin.integration.account.gov.uk.*')
     zap.spider.scan(target_url)
 
-    
-
     # Wait for the spider to finish
-
     time.sleep(5)
 
-    
-
     # Wait for passive scanning to complete
-
     while int(zap.pscan.records_to_scan) > 0:
-
         print(f"Records to passive scan: {zap.pscan.records_to_scan}")
-
         time.sleep(20)
 
-    
-
+    globalExcludeUrl = ['^(?:(?!http://localhost:8081).*).$']
     # Start active scanning
-
     print("Starting active scan…")
-
+  #  zap.ascan.exclude_from_scan('https://oidc.integration.account.gov.uk.*')
+  #  zap.ascan.exclude_from_scan('https://signin.integration.account.gov.uk.*')
     zap.ascan.scan(target_url)
 
     # Wait for the active scan to finish
 
     while int(zap.ascan.status()) < 100:
-#
         print(f"Active Scan progress: {zap.ascan.status()}%")
-
         time.sleep(50)
 
-    
-
     # Generate the report
-
     print("Generating HTML report…")
-
     sec_report_html = zap.core.htmlreport()
-
     with open("zap_report.html", "w") as file:
-
        file.write(sec_report_html)
-
-    
 
     print("Security scan completed successfully.")
 
-
-
     browser.close()
-
 
 with sync_playwright() as playwright:
     run(playwright)
