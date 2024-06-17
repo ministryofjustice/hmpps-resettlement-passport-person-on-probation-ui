5590ea4c29b3da6852e7fcdfff96c475a6a905d3
“DAVID MCCOURT <167427411+dmccourtzMoj@users.noreply.github.com>
2024-06-17 11:19:34 +0100

    added screenshot on each completed scenario - will catch all failures

--- E2E_Tests/hooks/hooks.ts
+++ E2E_Tests/hooks/hooks.ts
@@ -8,6 +8,7 @@ require('dotenv').config()
 let browser: Browser
 // let page: Page;
 let context: BrowserContext
+var count= 0;
 
 BeforeAll(async function () {
   browser = await chromium.launch({ headless: true }) // launches a chrome instance and returns a browser
@@ -24,6 +25,7 @@ Before(async function () {
 
 After(async function () {
   // After each scenario
+  await screenshotOnFailure();
   await pageFixture.page.close()
   await context.close()
 })
@@ -34,3 +36,16 @@ AfterAll(async function () {
   await browser.close()
   
 })
+
+
+async function screenshotOnFailure() {
+    // Get a unique place for the screenshot.
+    count++
+    const mytestID= 'TestNumber'+ count
+    console.log(mytestID)
+    
+    var screenshotPath = 'test_results/screenshots/'+mytestID+'.png'
+    // Take the screenshot itself.
+    await pageFixture.page.screenshot({ path: screenshotPath, timeout: 5000 });
+    
+}
