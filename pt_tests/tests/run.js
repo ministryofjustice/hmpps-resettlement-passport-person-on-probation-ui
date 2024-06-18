a5fdf99843119731e412ce8bc534ead9136afd03
“DAVID MCCOURT <167427411+dmccourtzMoj@users.noreply.github.com>
2024-06-18 13:38:37 +0100

    Update run.js

--- pt_tests/tests/run.js
+++ pt_tests/tests/run.js
@@ -17,7 +17,7 @@ export const options = {
     browser_http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     browser_http_req_duration: ['p(90)<2000', 'p(95)<5000'], // 90% of requests should be below 2000ms / 95% of requests should be below 5000ms
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    http_req_duration: ['p(95)<2000'], // 95% of requests should be below 2000ms
   },
 }
 
