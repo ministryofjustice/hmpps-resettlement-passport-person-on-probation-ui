fdee572ee7013dc65e0e69eba3ab7d3375de2569
dmccourtzMoj <167427411+dmccourtzMoj@users.noreply.github.com>
2024-06-18 14:00:57 +0100

    Test pt update (#170)
    * Update run.js
    
    * Update run.js

--- pt_tests/tests/run.js
+++ pt_tests/tests/run.js
@@ -15,9 +15,9 @@ export const options = {
   insecureSkipTLSVerify: true,
   thresholds: {
     browser_http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    browser_http_req_duration: ['p(90)<200', 'p(95)<500'], // 90% of requests should be below 200ms / 95% of requests should be below 500ms
+    browser_http_req_duration: ['p(90)<2000', 'p(95)<5000'], // 90% of requests should be below 2000ms / 95% of requests should be below 5000ms
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    http_req_duration: ['p(95)<2000'], // 95% of requests should be below 2000ms
   },
 }
 
