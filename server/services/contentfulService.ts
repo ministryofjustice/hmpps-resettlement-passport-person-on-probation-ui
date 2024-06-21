eb440438a902c997e6c236e0ef03104e490619d2
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-06-21 10:09:06 +0100

    PLT-315 Small fixes for Contentful elements

--- server/services/contentfulService.ts
+++ server/services/contentfulService.ts
@@ -91,7 +91,7 @@ const mapButton = (target: Target) => {
           <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z"></path>
         </svg>`
   }
-  return `<a href="${target?.fields?.link}" role="button" draggable="false" class="govuk-button govuk-button--start" data-module="govuk-button" data-qa="${target?.fields?.qatag}">
+  return `<a href="${target?.fields?.link}" role="button" draggable="false" class="govuk-button govuk-button--start govuk-!-margin-top-4" data-module="govuk-button" data-qa="${target?.fields?.qatag}">
         ${target?.fields?.text}${icon}</a>`
 }
 
