a25f5bd6cceb1d666758afd833141573fd0b2fd1
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-06-25 13:17:07 +0100

    PLT-310: fix e2e selector

--- E2E_Tests/pageObjects/dashboardPage.ts
+++ E2E_Tests/pageObjects/dashboardPage.ts
@@ -7,7 +7,7 @@ export default class DashboardPage {
   
     constructor(page: Page) {
       this.page = page;
-      this.pageHeader = page.locator('[data-qa="pyf-overview-title"]', { hasText: 'Plan your future'});
+      this.pageHeader = page.locator('h2:has-text("Plan your future")');
   
     }
   
