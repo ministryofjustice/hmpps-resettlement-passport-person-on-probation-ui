80ad2e69c4c4bf87c1aeca1451057a46b9bef04d
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-10-21 15:29:47 +0100

    Tidying

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,12 +1,11 @@
 const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 const completedTable = document.getElementById('completed-table')
-const cellStyle = 'govuk-table__cell'
 
-for (const form of document.querySelectorAll('form')) {
-  form.querySelector('input[type="checkbox"]').addEventListener('change', async e => {
+function handleCompleteResponse(form) {
+  return async (e) => {
     if (e.target.checked) {
       const res = await fetch(form.action, { method: 'POST', headers: { 'X-CSRF-Token': csrfToken } })
-      if(res.status === 200) {
+      if (res.status === 200) {
         const row = completedTable.insertRow(-1)
 
         const formData = new FormData(document.getElementById(form.id))
@@ -22,8 +21,12 @@ for (const form of document.querySelectorAll('form')) {
         completedAtCell.className = cellStyle
         const noteCell = row.insertCell(2)
         noteCell.innerHTML = formData.get('note')
-        noteCell.className = cellStyle
+        noteCell.className = `${cellStyle} govuk-body-s`
       }
     }
-  })
+  }
+}
+
+for (const form of document.querySelectorAll('form')) {
+  form.querySelector('input[type="checkbox"]').addEventListener('change', handleCompleteResponse(form))
 }
