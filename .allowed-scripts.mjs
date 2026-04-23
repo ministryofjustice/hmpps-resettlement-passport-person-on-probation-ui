420814bfe6f0b31fed97ac2e69484aacc146ae6b
mons kapinga <mons.kapinga1@justice.gov.uk>
2026-04-23 10:56:19 +0100

    Fix follow-redirects vulnerability

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,7 +5,6 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     'node_modules/cypress@14.5.1': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/unrs-resolver@1.7.2': 'ALLOW',
   },
 })
